b98fac8ae2fe543b47b2099c886b763c9ca67bbb
Sergio del Rio <sergio@xilix.io>
2024-02-06 19:28:09 +0000

    check & test

--- tests/check_test.sh
+++ tests/check_test.sh
@@ -4,10 +4,10 @@ current_os=$(uname -s)
 
 declare -A t=(
     [description]="Package is not installed on current OS"
-    [before_test]="echo 1"
+    [before_test]=""
     [test]="bin/df check pk1"
-    [after_test]="echo 2"
-    [expected_output]="Package 'pk1' is not installed on $current_os"
+    [after_test]=""
+    [expected_output]="Package 'pk1' is not installed on $current_os."
     [expected_status]=1
 ); run_test t
 
