f55b4fd75526f3fb45545aa2c99080e88b9da53a
Sergio del Rio <sergio@xilix.io>
2024-02-08 20:28:39 +0000

    Fix after_test behavior on t

--- tests/install-package_test.sh
+++ tests/install-package_test.sh
@@ -23,11 +23,11 @@ declare -A t=(
 
 declare -A t=(
     [description]="Install package and its dependencies"
-    [test]="bin/df install-package pk1 -y && \
+    [test]="bin/df install-package pk1 -y && 
             bin/df check pk1 -s && \
             bin/df check pk2 -s && \
             bin/df check pk3 -s"
-    [after_test]="rm /tmp/pk1.tmp && /tmp/pk2.tmp && /tmp/pk3.tmp"
+    [after_test]="rm /tmp/pk1.tmp || /tmp/pk2.tmp || /tmp/pk3.tmp || true"
     [expected_output]="Successfully installed package 'pk1' and its dependencies."
     [expected_status]=0
 ); run_test t
