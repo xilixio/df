823a4a43d95c4356159802022127e22f348bc81a
Sergio del Rio <sdelrio0@gmail.com>
2024-02-16 00:08:53 +0000

    fix test

--- tests/new_test.sh
+++ tests/new_test.sh
@@ -5,6 +5,7 @@ old_yaml="$DFM_YAML"
 
 DFM_YAML1=$(mktemp)
 DFM_YAML2=$(mktemp)
+DFM_YAML3=$(mktemp)
 
 yaml_contents="$(cat <<EOF
 packages:
@@ -12,6 +13,7 @@ EOF
 )"
 echo "$yaml_contents" > "$DFM_YAML1"
 echo "$yaml_contents" > "$DFM_YAML2"
+echo "$yaml_contents" > "$DFM_YAML3"
 
 declare -A t=(
     [description]="Add new package to yaml."
@@ -53,20 +55,12 @@ EOF
 
 declare -A t=(
     [description]="Add new package to yaml, create directory, and create executable."
-    [test]="DFM_YAML=$DFM_YAML2 bin/dfm new test -d && cat \"$DFM_YAML2\" && [ -d \"$DFM_DOTFILES/packages/test\" ] && [ -f \"$DFM_DOTFILES/packages/test\test.sh\" ]"
+    [test]="DFM_YAML=$DFM_YAML3 bin/dfm new test -df && [ -d \"$DFM_DOTFILES/packages/test\" ]  && [ -f \"$DFM_DOTFILES/packages/test/test.sh\" ]"
     [after_test]="rm -rf $DFM_DOTFILES/packages &> /dev/null"
     [expected_output]="$(cat <<EOF
-Added 'test' to '$DFM_YAML2'.
+Added 'test' to '$DFM_YAML3'.
 Created directory '$DFM_DOTFILES/packages/test'.
 Created executable '$DFM_DOTFILES/packages/test/test.sh'.
-packages:
-  test:
-    Linux:
-      install: null
-      check: null
-    Darwin:
-      install: null
-      check: null
 EOF
 )"
     [expected_status]=0
@@ -75,5 +69,6 @@ EOF
 # After all
 rm "$DFM_YAML1" >/dev/null 2>&1
 rm "$DFM_YAML2" >/dev/null 2>&1
+rm "$DFM_YAML3" >/dev/null 2>&1
 DFM_YAML="$old_yaml"
-rm -rf $DFM_DOTFILES/packages &> /dev/null
+rm -rf "$DFM_DOTFILES/packages" &> /dev/null
